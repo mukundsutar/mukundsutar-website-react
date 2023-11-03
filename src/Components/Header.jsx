62c38ab5b4c520139fd145e603d06dd423ae098b
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2023-11-03 09:42:39 +0530

    Started Personal website with react

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<>
+			<div className="navbar-container">
+				<div className="navbar-ele">Home</div>
+				<div className="navbar-ele">About Me</div>
+				<div className="navbar-ele">Skills</div>
+				<div className="navbar-ele">Resume</div>
+				<div className="navbar-ele">Projects</div>
+				<div className="navbar-ele">Contact</div>
+			</div>
+		</>
+	);
+}
